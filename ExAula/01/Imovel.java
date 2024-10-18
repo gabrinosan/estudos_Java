e3cdcf766a7c3428da4fb322856e561798d13fc2
Teste <gabrielsh1854@gmail.com>
2024-10-18 12:50:39 -0300

    Aplicação do Operador Tenário

--- ExAula/01/Imovel.java
+++ ExAula/01/Imovel.java
@@ -16,35 +16,8 @@ public class Imovel {
         im1.nBanheiros = 1;
         im1.temGaragem = false;
         im1.endereco = "Rua dos Fulanos, n. 0";
-        im1.aluguel = false;
+        im1.aluguel = true;
         im1.valorVenda = 142000;
-
-        /* Implementação da parte #5, conforme visto em aula os objetos não tem relação entre si, quando se instancia um novo objeto, 
-        este já é encapsulado, portanto, um objeto não é capaz de saber o que tem dentro do outro, o que corrobora para o resultado 
-        visto nesse item, "Imoveis distintos!" */
-
-        /* im2.nQuartos = 2;
-        im2.nBanheiros = 1;
-        im2.temGaragem = false;
-        im2.endereco = "Rua dos Fulanos, n. 0";
-        im2.aluguel = false;
-        im2.valorVenda = 142000;
-
-        if (im1 == im2){
-            System.out.println("Mesmos imoveis!");
-        }else{
-            System.out.println("Imoveis distintos!");
-        } */
-        
-        /* Parte #6 Aqui é possível ver um ponteiro valorVenda do im3 que aponta para im1, assim, quando mudo im3.valorVenda, este muda im1.valorVenda
-           Em python acontece a mesma coisa quando apontamos dessa forma, a solução seria usar o método copy(), assim mantenho íntegro o conteúdo de
-           im1.valorVenda, e posso mudar sem problemas im3.valorVenda */
-
-        /* Imovel im3 = im1;
-        im3.valorVenda = 0; // imovel com perda total
-        im3.imprimeDados();
-        System.out.println("\n");
-        im1.imprimeDados(); */
 
         System.out.println("--------------------------------------------------------------------------------");
         im1.imprimeDados();
@@ -63,11 +36,13 @@ public class Imovel {
                            "\nEndereço: " + endereco + 
                            "\nValor de venda: " + valorVenda);
 
-        if (temGaragem){System.out.println("Possui garagem");}
-        else{System.out.print("Não possui garagem\n");}
+        // Aplicação de um novo conceito, Operador de condição ternária
 
-        if (aluguel){System.out.println("Pode ser alugada");}
-        else{System.out.print("Não pode ser alugada\n");}
+        String verfica_garagem = temGaragem ? "Possui garagem\n" : "Não possui garagem\n";
+        System.out.print(verfica_garagem);
+
+        String verifica_aluguel = aluguel ? "Pode ser alugada\n" : "Não pode ser alugada\n";
+        System.out.print(verifica_aluguel);
     }
     
     public void valoriza(double perc){
